5427a321c8999d5028c8426c6287cb8c9baec01c
Dee <alwande.m18@gmail.com>
2026-08-03 00:23:38 +0200

    feat(database): create transactions migration schema

--- BackEnd/migrations/20260802213852_create_transactions_table.js
+++ BackEnd/migrations/20260802213852_create_transactions_table.js
@@ -2,14 +2,34 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-  
+exports.up = function (knex) {
+  return knex.schema.createTable("transactions", (table) => {
+    table.increments("id").primary();
+    table
+      .integer("user_id")
+      .unsigned()
+      .notNullable()
+      .references("id")
+      .inTable("users")
+      .onDelete("CASCADE");
+    table
+      .integer("category_id")
+      .unsigned()
+      .references("id")
+      .inTable("categories")
+      .onDelete("SET NULL"); // to keeps transaction history even if category is deleted
+    table.decimal("amount", 12, 2).notNullable(); // max 999,999,999.99 amount, 12 digits total, 2 decimal places
+    table.enum("type", ["income", "expense"]).notNullable();
+    table.date("date").notNullable();
+    table.string("description", 255);
+    table.timestamps(true, true);
+  });
 };
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists("transactions");
 };
