fafc85b86d35546ac0f3a7960222ebb2109a7fcb
Dee <alwande.m18@gmail.com>
2026-08-03 00:02:28 +0200

    feat(database): create categories migration schema

--- BackEnd/migrations/20260802213826_create_categories_table.js
+++ BackEnd/migrations/20260802213826_create_categories_table.js
@@ -2,14 +2,26 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-  
+exports.up = function (knex) {
+  return knex.schema.createTable("categories", (table) => {
+    table.increments("id").primary();
+    table
+      .integer("user_id")
+      .unsigned()
+      .notNullable()
+      .references("id")
+      .inTable("users")
+    table.string("name", 50).notNullable();
+    table.enum("type", ["income", "expense"]).notNullable();
+    table.string("color", 20).defaultTo("#4A90E2");
+    table.timestamps(true, true);
+  });
 };
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists("categories");
 };
