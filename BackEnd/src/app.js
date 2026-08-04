bd1929c4a5c894ee80bd964fa26636e818616760
Dee <alwande.m18@gmail.com>
2026-08-04 20:18:31 +0200

    feat(transactions): add transaction routes and update app configuration

--- BackEnd/src/app.js
+++ BackEnd/src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const categoryRoutes = require('./routes/categoryRoutes');
+const transactionRoutes = require('./routes/transactionRoutes');
 
 const app = express();
 
@@ -9,9 +10,11 @@ app.use(express.json());                                  // 1. Allow Express to
 
 app.use('/api/categories', categoryRoutes);               // 2. Connecting my category routes to the URL path /api/categories
 
+app.use('/api/transactions', transactionRoutes);
+
 
 app.listen(5000, () => {
   console.log('Server is running on http://localhost:5000');
 });
 
-//currently just testing my API's
+//currently just testing my API's    - http://localhost:5000/api/transactions
