3a64fbc456ba0292b8e8dfe18bf192fdb0151472
Dee <alwande.m18@gmail.com>
2026-08-04 14:35:59 +0200

    feat(app): set up Express server and connect category routes

---
+++ BackEnd/src/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const categoryRoutes = require('./routes/categoryRoutes');
+
+const app = express();
+
+
+app.use(express.json());                                  // 1. Allow Express to read JSON data sent to it coming from the frontend.  This is necessary for POST and PUT requests where the client sends data in the request body.  The express.json() middleware parses incoming JSON requests and puts the parsed data in req.body, making it accessible in your route handlers.
+
+
+app.use('/api/categories', categoryRoutes);               // 2. Connecting my category routes to the URL path /api/categories
+
+
+app.listen(5000, () => {
+  console.log('Server is running on http://localhost:5000');
+});
+
+//currently just testing my API's
